ab549a81e0bfa3a1a260d8128724ad0610b22917
AndyJrII <enehizenajames@gmail.com>
2024-10-11 22:42:56 +0100

    removed unneeded dependencies and admin related files

--- readme.md
+++
@@ -1,2 +0,0 @@
-Default password & username are both "admin" which can be edited in the .env file.
-Run the script "node createAdmin.js" to create an admin with the new username and password in the database.
--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//const passport = require('passport');
-//const session = require('express-session');
 const flash = require('connect-flash');
-//const initializePassport = require('./auth/passportConfig');
 const indexRoutes = require('./routes/index');
-//const adminRoutes = require('./routes/admin');
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -16,33 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
 
-// Passport
-//initializePassport(passport);
-
-/*
-app.use(
-  session({
-    secret: process.env.JWT_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
-*/
-
 // Use routes from the "routes" directory
 app.use('/', indexRoutes);
-//app.use('/admin', adminRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3001;
