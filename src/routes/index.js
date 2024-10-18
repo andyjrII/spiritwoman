71682ae7e76598418cb1df15f645c70af3d5160c
AndyJrII <enehizenajames@gmail.com>
2024-10-18 10:45:55 +0100

    created about page

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ router.get('/resources', (req, res) => res.render('resources'));
 router.get('/blog', (req, res) => res.render('blog'));
 router.get('/contact', (req, res) => res.render('contact'));
 router.post('/send-email', indexController.postSendMail);
+router.get('/about', (req, res) => res.render('about'));
 router.get('/privacy-policy', (req, res) => res.render('privacy_policy'));
 router.get('/terms-of-use', (req, res) => res.render('terms_of_use'));
 router.get('/copyright', (req, res) => res.render('copyright'));
