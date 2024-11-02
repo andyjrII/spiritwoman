54f5a2c044bdd99e2d0ae5a36d54cd2a786fc902
AndyJrII <enehizenajames@gmail.com>
2024-11-03 00:27:46 +0100

    moved server.js to root directory

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const indexRoutes = require('./routes/index');
+const indexRoutes = require('./src/routes/index');
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
